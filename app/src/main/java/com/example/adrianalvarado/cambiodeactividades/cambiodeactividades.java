3c0ef147523992ac6f3ef7e27628b78a94cbdb76
Adrian77990 <adbi_90@hotmail.com>
2016-08-03 12:27:54 -0600

    Alfaro_Clave2
    proyectos Alfaro 5to B

---
+++ app/src/main/java/com/example/adrianalvarado/cambiodeactividades/cambiodeactividades.java
@@ -0,0 +1,14 @@
+package com.example.adrianalvarado.cambiodeactividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cambiodeactividades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiodeactividades);
+
+    }
+}
